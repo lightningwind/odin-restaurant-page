a07b1013719390ff2d69ab01dd5f0191dc97b309
David Islam <david.islam24@gmail.com>
2023-06-16 14:43:42 -0700

    Set up the site to use tabbed browsing

--- src/pageLoad.js
+++ src/pageLoad.js
@@ -1,5 +1,7 @@
 import createRestaurantHomePage from './restaurant.js';
+import createTabs from './tabs.js';
 
 export default function loadPage() {
+  createTabs();
   createRestaurantHomePage();
 }
